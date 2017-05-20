95a96f6a3bbeffd9fd385fde22e7e1f99546a80b
u23a <browse@aliyun.com>
2017-05-20 09:46:08 +0800

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,54 @@
+
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Tweener = imports.ui.tweener;
+
+let text, button;
+
+function _hideHello() {
+    Main.uiGroup.remove_actor(text);
+    text = null;
+}
+
+function _showHello() {
+    if (!text) {
+        text = new St.Label({ style_class: 'helloworld-label', text:"人"+"\n"+"身体"+"\n"+"Program"+"\n"+"English"+"\n"+"手(Keyboard)"+"\n"+"嘴" });
+        Main.uiGroup.add_actor(text);
+    }
+
+    text.opacity = 255;
+
+    let monitor = Main.layoutManager.primaryMonitor;
+
+    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
+                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
+
+    Tweener.addTween(text,
+                     { opacity: 0,
+                       time: 2,
+                       transition: 'easeOutQuad',
+                       onComplete: _hideHello });
+}
+
+function init() {
+    button = new St.Bin({ style_class: 'panel-button',
+                          reactive: true,
+                          can_focus: true,
+                          x_fill: true,
+                          y_fill: false,
+                          track_hover: true });
+    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
+                             style_class: 'system-status-icon' });
+
+    let label=new St.Label({ text:"moon" });
+    button.set_child(label);
+    button.connect('button-press-event', _showHello);
+}
+
+function enable() {
+    Main.panel._leftBox.insert_child_at_index(button,-1);
+}
+
+function disable() {
+    Main.panel._rightBox.remove_child(button);
+}
